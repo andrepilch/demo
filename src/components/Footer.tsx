55f236dbb4fc3067cd94b5a660d4915307ef0788
André Pilch <andre.pilch@whcc.com>
2026-01-12 07:38:14 -0600

    Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="px-8 py-6 border-t border-neutral-200 dark:border-neutral-800 text-center text-neutral-500 text-sm">
+    <footer className="px-8 py-6 text-center text-neutral-500 text-sm">
       <p>©2009-{currentYear} André Pilch</p>
     </footer>
   );
